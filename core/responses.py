a2210f2157d21b8bd10f806efc911e08ba98f186
Exybore <exybore@gmail.com>
2019-04-08 19:04:52 +0200

    feat: show optional data
    An invalid request for data error will now display optional data that can be added to the request's body.

--- core/responses.py
+++ core/responses.py
@@ -12,11 +12,12 @@ def invalid_username_or_password():
     }), 400, {"Content-Type": "application/json"}
 
 
-def data_error(required_data):
+def data_error(required_data, optional_data = {}):
     return json.dumps({
         "code": 0,
         "message": "Data error",
-        "required_data": required_data
+        "required_data": required_data,
+        "optional_data": optional_data
     }), 400, {"Content-Type": "application/json"}
 
 
