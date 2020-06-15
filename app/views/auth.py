1984add7f8ff72d61e03ffa0c1e60afb7a3acc4b
Théo Vidal <exybore@becauseofprog.fr>
2020-06-15 19:40:32 +0200

    feat: add data getting by token
    Used for reconnections on the blog

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,7 +1,9 @@
 from flask import request
 from werkzeug.exceptions import NotFound
 
+from app.controllers.users import UsersController
 from app.controllers.auth import AuthController
+from app.middlewares.auth import CheckAuth
 from app.middlewares.body import CheckBody
 from core import responses
 from core.exceptions import InvalidCreds, DataError
@@ -30,3 +32,12 @@ def create_session():
         return responses.data_error(required_data)
     except InvalidCreds:
         return responses.invalid_username_or_password()
+
+
+@app.route('/v1/auth/data', methods=['GET'])
+def get_information():
+    CheckAuth(request)
+    return responses.response({
+        'code': 1,
+        'data': UsersController.get_one_by_token(request.headers.get('Authorization'))
+    })
